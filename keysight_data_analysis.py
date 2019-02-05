caab90e8e9e3bce9e80ddb216d90b194464d9396
jmajor <joshua.major@nrel.gov>
2019-02-05 14:07:24 -0700

    commit

--- keysight_data_analysis.py
+++ keysight_data_analysis.py
@@ -16,12 +16,12 @@ def signaltonoise(a, axis=0, ddof=0):
 
 #file =  'C:/Users/jmajor/Desktop/github/Bat_cal/Data/Office_insulation_just moved Feb 05 2019, time_10_26_56.csv'
 
-file = 'C:/Users/jmajor/Desktop/github/Battery_cal_/Data/current_cooldown_1 Feb 05 2019, time_11_50_04.csv'
+file = 'C:/Users/jmajor/Desktop/github/Battery_cal_/TIM_data/TIM_current_test_2 Feb 05 2019, time_12_22_38.csv'
 
 df = pd.read_csv(file)
 
 data_range_high = None
-range_low = None
+range_low = 100
 
 times = df['Time'][range_low:data_range_high]
 volts = df['Voltage'][range_low:data_range_high]
